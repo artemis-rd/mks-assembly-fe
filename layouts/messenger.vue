7c895ae282def64bb7ec88b05c71c542d4db4f06
benjamin-23 <kitongabenja34@gmail.com>
2022-10-31 16:09:21 +0300

    direct message

--- layouts/messenger.vue
+++ layouts/messenger.vue
@@ -1,14 +1,31 @@
 <script setup>
- 
+
 </script>
 <template>
     <div class="flex">
-        <div class=""> 
-            <h1 class="text-lg font-bold my-5">Messenger</h1>
-           <SearchInput/>   
+        <div class="ml-6 my-3 mb-0 container">
+            <h2 class="text-lg font-bold my-5 mb-2">Messenger</h2>
+            <SearchInput />
+
+            <!-- direct messages -->
+            <div class="my-1 text-sm">
+                <p class="my-5 font-bold text-xs">Direct Messages</p>
+                <div class="flex gap-2">
+                    <img src="@/assets/img/profile.png" alt="loading">
+                    <div class="flex-col">
+                        <div class="flex justify-between">
+                            <p>Baraka sean</p>
+                            <p>4.14 p.m</p>
+                        </div>
+                        <p class=" text-xs">Lorem ipsum dolor sit amet consectetur adipisicing elit....</p>
+                    </div>
+                </div>
+
+            </div>
+
+
         </div>
-    
         <slot></slot>
     </div>
-   
+
 </template>
