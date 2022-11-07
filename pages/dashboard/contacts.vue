8156fbf3c046058d53394b274ffa29c329a6e501
jonny <gatherujohn11@gmail.com>
2022-11-07 09:09:09 +0300

    contacts

--- pages/dashboard/contacts.vue
+++ pages/dashboard/contacts.vue
@@ -1,3 +1,295 @@
 <template>
-    <h2>Contacts</h2>
+  <div class="">
+    <TopBar />
+    <div class="absolute bg-slate-500 w-11/12 h-full"></div>
+
+    <div class="pt-5 px-4">
+      <div class="flex justify-between items-center">
+        <div
+          class="flex w-8/12 justify-between border rounded-lg outline-none py-2 px-2"
+        >
+          <input
+            class="outline-none text-sm w-full"
+            type="text"
+            placeholder="Search Contacts By either Name or Phone Number"
+          />
+          <img class="" src="@/assets/img/Search.svg" />
+        </div>
+        <button
+          class="flex items-center justify-center gap-2 font-medium rounded-md py-2 px-4 text-white text-sm bg-orange-500"
+        >
+          <img src="@/assets/img/AddUser.svg" alt="" /> Add a New Contact
+        </button>
+      </div>
+    </div>
+    <div class="font-bold px-4 mt-2">A</div>
+    <div class="px-4 grid grid-cols-3 gap-3">
+      <div class="flex mt-3 gap-2">
+        <div class="w-9 h-9 rounded-full bg-orange">
+          <img src="@/assets/img/avatarMessage.svg" alt="" width="100" />
+        </div>
+        <div class="flex-col">
+          <div class="flex justify-between">
+            <div class="text-sm font-bold">ICT Office</div>
+            <div class="text-sm font-bold">2.49pm</div>
+          </div>
+          <div class="text-xs text-slate-400">
+            Can you confirm whether internet services are....
+          </div>
+        </div>
+      </div>
+      <div class="flex mt-3 gap-2">
+        <div class="w-9 h-9 rounded-full bg-orange">
+          <img src="@/assets/img/avatarMessage.svg" alt="" width="100" />
+        </div>
+        <div class="flex-col">
+          <div class="flex justify-between">
+            <div class="text-sm font-bold">ICT Office</div>
+            <div class="text-sm font-bold">2.49pm</div>
+          </div>
+          <div class="text-xs text-slate-400">
+            Can you confirm whether internet services are....
+          </div>
+        </div>
+      </div>
+      <div class="flex mt-3 gap-2">
+        <div class="w-9 h-9 rounded-full bg-orange">
+          <img src="@/assets/img/avatarMessage.svg" alt="" width="100" />
+        </div>
+        <div class="flex-col">
+          <div class="flex justify-between">
+            <div class="text-sm font-bold">ICT Office</div>
+            <div class="text-sm font-bold">2.49pm</div>
+          </div>
+          <div class="text-xs text-slate-400">
+            Can you confirm whether internet services are....
+          </div>
+        </div>
+      </div>
+      <div class="flex mt-3 gap-2">
+        <div class="w-9 h-9 rounded-full bg-orange">
+          <img src="@/assets/img/avatarMessage.svg" alt="" width="100" />
+        </div>
+        <div class="flex-col">
+          <div class="flex justify-between">
+            <div class="text-sm font-bold">ICT Office</div>
+            <div class="text-sm font-bold">2.49pm</div>
+          </div>
+          <div class="text-xs text-slate-400">
+            Can you confirm whether internet services are....
+          </div>
+        </div>
+      </div>
+      <div class="flex mt-3 gap-2">
+        <div class="w-9 h-9 rounded-full bg-orange">
+          <img src="@/assets/img/avatarMessage.svg" alt="" width="100" />
+        </div>
+        <div class="flex-col">
+          <div class="flex justify-between">
+            <div class="text-sm font-bold">ICT Office</div>
+            <div class="text-sm font-bold">2.49pm</div>
+          </div>
+          <div class="text-xs text-slate-400">
+            Can you confirm whether internet services are....
+          </div>
+        </div>
+      </div>
+      <div class="flex mt-3 gap-2">
+        <div class="w-9 h-9 rounded-full bg-orange">
+          <img src="@/assets/img/avatarMessage.svg" alt="" width="100" />
+        </div>
+        <div class="flex-col">
+          <div class="flex justify-between">
+            <div class="text-sm font-bold">ICT Office</div>
+            <div class="text-sm font-bold">2.49pm</div>
+          </div>
+          <div class="text-xs text-slate-400">
+            Can you confirm whether internet services are....
+          </div>
+        </div>
+      </div>
+      <div class="flex mt-3 gap-2">
+        <div class="w-9 h-9 rounded-full bg-orange">
+          <img src="@/assets/img/avatarMessage.svg" alt="" width="100" />
+        </div>
+        <div class="flex-col">
+          <div class="flex justify-between">
+            <div class="text-sm font-bold">ICT Office</div>
+            <div class="text-sm font-bold">2.49pm</div>
+          </div>
+          <div class="text-xs text-slate-400">
+            Can you confirm whether internet services are....
+          </div>
+        </div>
+      </div>
+      <div class="flex mt-3 gap-2">
+        <div class="w-9 h-9 rounded-full bg-orange">
+          <img src="@/assets/img/avatarMessage.svg" alt="" width="100" />
+        </div>
+        <div class="flex-col">
+          <div class="flex justify-between">
+            <div class="text-sm font-bold">ICT Office</div>
+            <div class="text-sm font-bold">2.49pm</div>
+          </div>
+          <div class="text-xs text-slate-400">
+            Can you confirm whether internet services are....
+          </div>
+        </div>
+      </div>
+      <div class="flex mt-3 gap-2">
+        <div class="w-9 h-9 rounded-full bg-orange">
+          <img src="@/assets/img/avatarMessage.svg" alt="" width="100" />
+        </div>
+        <div class="flex-col">
+          <div class="flex justify-between">
+            <div class="text-sm font-bold">ICT Office</div>
+            <div class="text-sm font-bold">2.49pm</div>
+          </div>
+          <div class="text-xs text-slate-400">
+            Can you confirm whether internet services are....
+          </div>
+        </div>
+      </div>
+      <div class="flex mt-3 gap-2">
+        <div class="w-9 h-9 rounded-full bg-orange">
+          <img src="@/assets/img/avatarMessage.svg" alt="" width="100" />
+        </div>
+        <div class="flex-col">
+          <div class="flex justify-between">
+            <div class="text-sm font-bold">ICT Office</div>
+            <div class="text-sm font-bold">2.49pm</div>
+          </div>
+          <div class="text-xs text-slate-400">
+            Can you confirm whether internet services are....
+          </div>
+        </div>
+      </div>
+      <div class="flex mt-3 gap-2">
+        <div class="w-9 h-9 rounded-full bg-orange">
+          <img src="@/assets/img/avatarMessage.svg" alt="" width="100" />
+        </div>
+        <div class="flex-col">
+          <div class="flex justify-between">
+            <div class="text-sm font-bold">ICT Office</div>
+            <div class="text-sm font-bold">2.49pm</div>
+          </div>
+          <div class="text-xs text-slate-400">
+            Can you confirm whether internet services are....
+          </div>
+        </div>
+      </div>
+      <div class="flex mt-3 gap-2">
+        <div class="w-9 h-9 rounded-full bg-orange">
+          <img src="@/assets/img/avatarMessage.svg" alt="" width="100" />
+        </div>
+        <div class="flex-col">
+          <div class="flex justify-between">
+            <div class="text-sm font-bold">ICT Office</div>
+            <div class="text-sm font-bold">2.49pm</div>
+          </div>
+          <div class="text-xs text-slate-400">
+            Can you confirm whether internet services are....
+          </div>
+        </div>
+      </div>
+      <div class="flex mt-3 gap-2">
+        <div class="w-9 h-9 rounded-full bg-orange">
+          <img src="@/assets/img/avatarMessage.svg" alt="" width="100" />
+        </div>
+        <div class="flex-col">
+          <div class="flex justify-between">
+            <div class="text-sm font-bold">ICT Office</div>
+            <div class="text-sm font-bold">2.49pm</div>
+          </div>
+          <div class="text-xs text-slate-400">
+            Can you confirm whether internet services are....
+          </div>
+        </div>
+      </div>
+      <div class="flex mt-3 gap-2">
+        <div class="w-9 h-9 rounded-full bg-orange">
+          <img src="@/assets/img/avatarMessage.svg" alt="" width="100" />
+        </div>
+        <div class="flex-col">
+          <div class="flex justify-between">
+            <div class="text-sm font-bold">ICT Office</div>
+            <div class="text-sm font-bold">2.49pm</div>
+          </div>
+          <div class="text-xs text-slate-400">
+            Can you confirm whether internet services are....
+          </div>
+        </div>
+      </div>
+      <div class="flex mt-3 gap-2">
+        <div class="w-9 h-9 rounded-full bg-orange">
+          <img src="@/assets/img/avatarMessage.svg" alt="" width="100" />
+        </div>
+        <div class="flex-col">
+          <div class="flex justify-between">
+            <div class="text-sm font-bold">ICT Office</div>
+            <div class="text-sm font-bold">2.49pm</div>
+          </div>
+          <div class="text-xs text-slate-400">
+            Can you confirm whether internet services are....
+          </div>
+        </div>
+      </div>
+      <div class="flex mt-3 gap-2">
+        <div class="w-9 h-9 rounded-full bg-orange">
+          <img src="@/assets/img/avatarMessage.svg" alt="" width="100" />
+        </div>
+        <div class="flex-col">
+          <div class="flex justify-between">
+            <div class="text-sm font-bold">ICT Office</div>
+            <div class="text-sm font-bold">2.49pm</div>
+          </div>
+          <div class="text-xs text-slate-400">
+            Can you confirm whether internet services are....
+          </div>
+        </div>
+      </div>
+      <div class="flex mt-3 gap-2">
+        <div class="w-9 h-9 rounded-full bg-orange">
+          <img src="@/assets/img/avatarMessage.svg" alt="" width="100" />
+        </div>
+        <div class="flex-col">
+          <div class="flex justify-between">
+            <div class="text-sm font-bold">ICT Office</div>
+            <div class="text-sm font-bold">2.49pm</div>
+          </div>
+          <div class="text-xs text-slate-400">
+            Can you confirm whether internet services are....
+          </div>
+        </div>
+      </div>
+      <div class="flex mt-3 gap-2">
+        <div class="w-9 h-9 rounded-full bg-orange">
+          <img src="@/assets/img/avatarMessage.svg" alt="" width="100" />
+        </div>
+        <div class="flex-col">
+          <div class="flex justify-between">
+            <div class="text-sm font-bold">ICT Office</div>
+            <div class="text-sm font-bold">2.49pm</div>
+          </div>
+          <div class="text-xs text-slate-400">
+            Can you confirm whether internet services are....
+          </div>
+        </div>
+      </div>
+      <div class="flex mt-3 gap-2">
+        <div class="w-9 h-9 rounded-full bg-orange">
+          <img src="@/assets/img/avatarMessage.svg" alt="" width="100" />
+        </div>
+        <div class="flex-col">
+          <div class="flex justify-between">
+            <div class="text-sm font-bold">ICT Office</div>
+            <div class="text-sm font-bold">2.49pm</div>
+          </div>
+          <div class="text-xs text-slate-400">
+            Can you confirm whether internet services are....
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
