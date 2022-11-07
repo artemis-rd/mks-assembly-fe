3042f468c3b2508d89c61038b3757ce1d408cb2c
benjamin-23 <kitongabenja34@gmail.com>
2022-11-07 16:47:39 +0300

    messages links

--- components/TopBar.vue
+++ components/TopBar.vue
@@ -1,8 +1,17 @@
+<script setup lang="ts">
+import { use } from 'h3';
+
+defineProps({
+  name:String,
+  lastLogin:String,
+  user:String
+})
+</script>
 <template>
-  <div class="flex justify-between py-2 border-b border-neutral-100 px-4">
+  <div class="flex justify-between py-4 border-b border-neutral-100 px-4">
     <div class="">
-      <h6 class="font-bold">Availlable Contacts</h6>
-      <div class="text-xs">278 Contacts</div>
+      <h6 class="font-bold text-xs">{{name}}</h6>
+      <div class="text-xs">{{lastLogin}}</div>
     </div>
     <div class="flex justify-between items-center gap-8 pr-4">
       <img src="@/assets/img/Search.svg" alt="" />
@@ -10,7 +19,7 @@
 
       <div class="flex items-center gap-2">
         <img src="@/assets/img/avatartop.svg" alt="" />
-        <div class="text-sm">Angel mwende</div>
+        <div class="text-xs font-bold">{{user}}</div>
       </div>
     </div>
   </div>
