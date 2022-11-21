9ef7df94113de9b4624f89abd67b91aa0a1d184f
benjamin-23 <kitongabenja34@gmail.com>
2022-11-21 16:32:28 +0300

    more table for data

--- data/db.ts
+++ data/db.ts
@@ -8,15 +8,27 @@ export interface UserMessages{
     receiver?:number,
     roomId?:number
 }
-
+export interface Rooms{
+    id?:number
+    startTime?:string,
+    participants?:string
+}
+export interface User { 
+    id?:number,
+    name?:string
+}
 
 export class MySubClassedDexie extends Dexie {
   userMessages!: Table<UserMessages>;
+  rooms!:Table<Rooms>;
+  user!:Table<User>;
 
   constructor(){
     super('messageDatabase');
-    this.version(1).stores({
-        userMessages:'++id, message, timestamp'
+    this.version(2).stores({
+        userMessages:'++id, message, timestamp',
+        rooms:'id, startTime, participants',
+        user:'id,name'
     })
   }
 }
