d34e24866afb56ce6533119612aba1f5bd5ee59e
benjamin-23 <kitongabenja34@gmail.com>
2022-11-30 16:02:07 +0300

    hover effect color

--- components/MenuItem.vue
+++ components/MenuItem.vue
@@ -4,7 +4,7 @@ defineProps({
 });
 </script>
 <template>
-  <NuxtLink :to="to" class="menu-item block py-2 px-7 cursor-pointer">
+  <NuxtLink :to="to" class="menu-item block py-4 px-7 cursor-pointer hover:bg-orange-100 hover:border-r-[3px] hover:border-orange-500">
     <div class="flex gap-2 items-center text-sm font-semibold">
       <slot></slot>
     </div>
