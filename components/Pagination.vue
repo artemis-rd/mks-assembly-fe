fd8ea0eadfec065b774b6c679fba58ae749d2781
benjamin-23 <kitongabenja34@gmail.com>
2022-12-06 01:33:57 +0300

    props, emitter for next and previous button

--- components/Pagination.vue
+++ components/Pagination.vue
@@ -1,58 +1,48 @@
 <script setup lang="ts">
 import { Ref } from 'vue';
 
-const {
-  public: { MESSAGING_SERVICE },
-} = useRuntimeConfig();
-const listPayment = useState("receivedPayment")
-const count = [
-    1, 2, 3, 4
-]
-const list:Ref<any[]> = ref([])
+const props = defineProps({
+  currentPage: Number,
+  totalPages: Number
+})
 
-const currentPage = ref()
-const pageSize = ref(6)
-const  { data: paymentList } = await useFetch<any>(`${MESSAGING_SERVICE}/payment/list`,{
-    method:"GET",
-    key: Math.ceil(Math.random() * 1000).toString(),
-    params: {
-        page:currentPage.value,
-        take:pageSize.value
+const emit = defineEmits(['goToPage'])
+
+const previousPage = () => {
+    let p = props.currentPage - 1;
+    if (p > 1) {
+        emit('goToPage', p);
+    } else emit('goToPage', 1)
+}
+
+const nextPage = () => {
+    let n = props.currentPage + 1;
+    if (n > props.totalPages) {
+        emit('goToPage', 1)
     }
-}
-)
-console.log('the list of payments', paymentList.value);
-const numberOfPages = ref()
-// listPayment.value = paymentList.value
-// console.log(listPayment.value, "payment list");
-
-
-function prevPage(){
-currentPage.value++
-}
-function nextPage(){
+    else
+    emit('goToPage', n) // test alaf unishow kama iko sawa
 }
 
 </script>
 <template>
     <div class="  absolute bottom-0 border-t-[1px] w-full border-gray-500">
         <div class="flex gap-2 items-center mt-2">
-            {{paymentList}}
-            <div class="flex gap-2 font-bold" v-for="transaction in paymentList.paymentsList">
+            <div class="flex gap-2 font-bold" >
                 <span>Showing</span>
-                <span>{{transaction.page}}</span>
+                <span :currentPage="currentPage">{{}}</span>
                 <span>of</span>
-                <span>{{transaction.paymentsCount}}</span>
+                <span :totalPages="totalPages">{{}}</span>
             </div>
             <!-- previous -->
             <div class="flex gap-2">
                 <button 
-                    @click="prevPage()"
+                    @click="previousPage"
                     class="flex items-center gap-2 bg-orange-200 px-4 py-2 rounded-lg text-orange-500 font-semibold">
                     <img class="w-2" src="@/assets/img/previousIcon.svg" alt="loading">
                     <span>Previous</span>
                 </button>
-                <button @click="nextPage()" class="flex items-center gap-2 bg-orange-200 px-4 rounded-lg text-orange-500 font-semibold">
+                <button @click="nextPage" class="flex items-center gap-2 bg-orange-200 px-4 rounded-lg text-orange-500 font-semibold">
                     <span>Next</span>
                     <img class="w-2" src="@/assets/img/nextIcon.svg" alt="loading">
                 </button>
@@ -60,7 +50,7 @@ function nextPage(){
             </div>
             <NuxtLink
                 class=" hover:bg-orange-500 font-semibold hover:rounded-[100%] hover:w-4 p-[4px] hover:text-blue-50"
-                v-for="pageCount in count" key="pageCount">{{ pageCount }}</NuxtLink>
+                v-for="pageCount in totalPages" key="pageCount">{{ pageCount }}</NuxtLink>
 
         </div>
     </div>
