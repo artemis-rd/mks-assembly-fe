bcd196e5d87e343ca29b45e1bdae5f4c77f2b4af
Benjamin-23 <66298280+Benjamin-23@users.noreply.github.com>
2022-12-07 18:15:57 +0300

    Update billing.vue

--- pages/dashboard/billing.vue
+++ pages/dashboard/billing.vue
@@ -22,7 +22,7 @@ const { data: paymentList, refresh: refreshList } = await useFetch<any>(
       take: pageSize.value,
     },
   }
-})
+);
 const paymentCount = Math.ceil(paymentList.value.paymentsCount/ pageSize.value)
 watch(currentPage, async (newVal) => {
   const { data } = await useFetch<any>(`${MESSAGING_SERVICE}/payment/list`, {
