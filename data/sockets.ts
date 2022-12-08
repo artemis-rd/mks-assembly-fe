2eb26d33e4ceab52d1b893c11829aa4feddcd4fd
seanbaraka <seanbaraka@gmail.com>
2022-12-08 16:27:11 +0300

    fix: bug fix

--- data/sockets.ts
+++ data/sockets.ts
@@ -1,5 +1,5 @@
 import { io , Socket } from "socket.io-client";
 
-const { public: MESSAGING_SOCKET_URL } = useRuntimeConfig();
+const { public: { MESSAGING_SOCKET_URL }  } = useRuntimeConfig();
 
 export const socket: Socket = io(`${MESSAGING_SOCKET_URL}`);
